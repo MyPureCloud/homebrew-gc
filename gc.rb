17921c8c6dd7b32c83c3f240cad161d88800b7bd
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2021-06-22 17:32:54 +0000

    14.0.0

--- gc.rb
+++ gc.rb
@@ -1,7 +1,7 @@
 class Gc < Formula
   desc "A standalone CLI package to interface with the Genesys Cloud Platform API"
-  url "https://github.com/MyPureCloud/platform-client-sdk-cli/archive/13.0.0.tar.gz"
-  sha256 "66ae73c84db360065c5b85c11fb5bab37549db9ac079112e973328497eb729ce"
+  url "https://github.com/MyPureCloud/platform-client-sdk-cli/archive/14.0.0.tar.gz"
+  sha256 "d083ce748b93c8ad21953e355e0df8dd0b155156a5068408acfd9ce663ac67f8"
   license "MIT"
 
   depends_on "go" => :build
